aa84d2523523f74176ec6bc7ee72d5bdfb045912
Mroik <mroik@delayed.space>
2023-05-12 00:58:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+For the cookie file you can use [cookie-editor](https://cookie-editor.cgagnier.ca/).
+When yuo extract use the JSON format.
---
+++ main.py
@@ -0,0 +1,74 @@
+from requests import Session
+from argparse import ArgumentParser
+from json import loads as to_json
+from uuid import uuid4
+
+
+def load_cookies(opts):
+    with open(opts.c, "r") as fd:
+        temp = fd.read()
+        temp = to_json(temp)
+        opts.cook = {}
+        for cc in temp:
+            opts.cook[cc["name"]] = cc["value"]
+
+
+def set_shortcode(opts):
+    temp = opts.url[8:]
+    opts.shortcode = temp.split("/")[2]
+
+
+def main(opts):
+    sess = Session()
+    sess.get(opts.url)
+    
+    cookies = opts.cook
+    
+    headers = {
+        'authority': 'www.instagram.com',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.8',
+        'content-type': 'application/x-www-form-urlencoded',
+        'origin': 'https://www.instagram.com',
+        'sec-ch-ua': '"Brave";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'sec-ch-ua-platform-version': '"6.3.0"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'sec-gpc': '1',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+    }
+    
+    data = {
+        'fb_dtsg': 'NAcOyt0pNavIU9RHkaLXuFFSFEvvR-mbjsaIkMso6ahj1kp-HpYObsA:17864955220006059:1683609775',
+        'fb_api_caller_class': 'RelayModern',
+        'variables': '{"shortcode": "' + opts.shortcode + '"}',
+        'doc_id': '6129119620513740',
+    }
+    
+    response = sess.post('https://www.instagram.com/api/graphql', cookies=cookies, headers=headers, data=data)
+    data = response.json()
+
+    dash = data["data"]["xdt_api__v1__media__shortcode__web_info"]["items"][0]["video_dash_manifest"]
+    if dash is not None:
+        with open(str(uuid4()), "w") as fd:
+            fd.write(dash)
+    for item in data["data"]["xdt_api__v1__media__shortcode__web_info"]["items"][0]["carousel_media"]:
+        if item["video_dash_manifest"] is not None:
+            with open(str(uuid4()), "w") as fd:
+                fd.write(item["video_dash_manifest"])
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-c", help="Cookie file", type=str, required=True)
+    parser.add_argument("url", help="URL of a post/reel")
+
+    opts = parser.parse_args()
+    if opts.c:
+        load_cookies(opts)
+    set_shortcode(opts)
+
+    main(opts)
